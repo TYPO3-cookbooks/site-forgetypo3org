e23a2fd7a69813bc5ee394a63faa992b972d6c5f
Steffen Gebert <steffen.gebert@typo3.org>
2013-02-18 01:20:43 +0100

    Add parts of PHP configuration needed for SSO

--- recipes/default.rb
+++ recipes/default.rb
@@ -24,3 +24,29 @@
 }.each do |pkg|
   package pkg
 end
+
+####################################
+# nginx
+####################################
+
+# replace the nginx-site file
+
+chef_gem "chef-rewind"
+require 'chef/rewind'
+
+include_recipe "redmine::nginx"
+
+rewind :template => "/etc/nginx/sites-available/#{node.redmine.hostname}" do
+  cookbook_name "site-forgetypo3org"
+end
+
+template "/etc/nginx/redirects.conf" do
+  source "nginx/redirects.erb"
+  notifies :reload, "service[nginx]"
+end
+
+####################################
+# php
+####################################
+
+include_recipe "php"
