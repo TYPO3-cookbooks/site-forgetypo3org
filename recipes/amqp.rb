9d43086db64747efd2a03b37c438a22e71c20359
Steffen Gebert <steffen.gebert@typo3.org>
2013-10-02 00:18:54 +0200

    Overwork amqp part to use chef-vault

--- recipes/amqp.rb
+++ recipes/amqp.rb
@@ -1,26 +1,25 @@
-if Chef::Config[:solo]
-  Chef::Log.warn "AMQP connection will be disabled as running inside of chef-solo!"
-  amqp_pass = "fooo"
+if not node['site-forgetypo3org']['amqp']['user'].nil? then
+  amqp_pass = node['site-forgetypo3org']['amqp']['user']
 
 else
-  Chef::Log.warn "AMQP password handling is a mess, Steffen @todo"
 
-  # remove pre- from environment (if we're eg in pre-production, which almost equals production)
-  #environment = node.chef_environment.gsub(/pre-/, '')
+  if Chef::Config[:solo]
+    Chef::Log.warn "AMQP connection will be disabled as running inside of chef-solo!"
+    amqp_pass = "fooo"
+  else
 
-  # get all passwords for this environment
-  #  all_password_data = Chef::EncryptedDataBagItem.load("passwords", environment)
+    # read AMQP password from chef-vault
+    chef_gem "chef-vault"
+    require 'chef-vault'
 
-  # remove . from fqdn
-  #server_cleaned = node['amqp']['server'].gsub(/\./, "")
-  #user = node['amqp']['user']
+    amqp_server_cleaned = node['site-forgetypo3org']['amqp']['server'].gsub(/\./, '')
+    amqp_user = node['site-forgetypo3org']['amqp']['user']
+    # look for the vault entry like mqtypo3org-forgetypo3org in the passwords data bag
+    vault = ChefVault::Item.load("passwords", "#{amqp_server_cleaned}-#{amqp_user}")
+    amqp_pass = vault['amqp_user']
 
-  #Chef::Log.info "Looking for password rabbitmq.#{server_cleaned}.#{}"
-#  if all_password_data["rabbitmq"][server_cleaned][user].nil?
-#    raise "Cannot find password for rabbitmq user '#{user}' in data bag 'passwords/#{node["chef_environment"]}'."
-#  end
+  end
 
-#  amqp_pass = all_password_data["rabbitmq"][server_cleaned][user]
 end
 
 template "#{node['redmine']['deploy_to']}/shared/config/amqp.yml" do
@@ -29,9 +28,9 @@ template "#{node['redmine']['deploy_to']}/shared/config/amqp.yml" do
   group "redmine"
   mode "0664"
   variables(
-      :server    => node['amqp']['server'],
-      :username  => node['amqp']['user'],
-      :password  => node['amqp']['password'],
-      :vhost     => node['amqp']['vhost']
+    :server    => node['site-forgetypo3org']['amqp']['server'],
+    :username  => node['site-forgetypo3org']['amqp']['user'],
+    :password  => amqp_pass,
+    :vhost     => node['site-forgetypo3org']['amqp']['vhost']
   )
 end
