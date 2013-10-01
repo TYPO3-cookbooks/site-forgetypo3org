72a6056fb09c7a17b5ae8263101a292937fb3d50
Peter Niederlag <netservice@niekom.de>
2013-10-01 18:22:53 +0200

    amqp attribute/password handling, @todo improve

--- recipes/amqp.rb
+++ recipes/amqp.rb
@@ -3,23 +3,24 @@ if Chef::Config[:solo]
   amqp_pass = "fooo"
 
 else
+  Chef::Log.warn "AMQP password handling is a mess, Steffen @todo"
 
   # remove pre- from environment (if we're eg in pre-production, which almost equals production)
-  environment = node.chef_environment.gsub(/pre-/, '')
+  #environment = node.chef_environment.gsub(/pre-/, '')
 
   # get all passwords for this environment
-  all_password_data = Chef::EncryptedDataBagItem.load("passwords", environment)
+  #  all_password_data = Chef::EncryptedDataBagItem.load("passwords", environment)
 
   # remove . from fqdn
-  server_cleaned = node['amqp']['server'].gsub(/\./, "")
-  user = node['amqp']['user']
+  #server_cleaned = node['amqp']['server'].gsub(/\./, "")
+  #user = node['amqp']['user']
 
-  Chef::Log.info "Looking for password rabbitmq.#{server_cleaned}.#{}"
-  if all_password_data["rabbitmq"][server_cleaned][user].nil?
-    raise "Cannot find password for rabbitmq user '#{user}' in data bag 'passwords/#{node["chef_environment"]}'."
-  end
+  #Chef::Log.info "Looking for password rabbitmq.#{server_cleaned}.#{}"
+#  if all_password_data["rabbitmq"][server_cleaned][user].nil?
+#    raise "Cannot find password for rabbitmq user '#{user}' in data bag 'passwords/#{node["chef_environment"]}'."
+#  end
 
-  amqp_pass = all_password_data["rabbitmq"][server_cleaned][user]
+#  amqp_pass = all_password_data["rabbitmq"][server_cleaned][user]
 end
 
 template "#{node['redmine']['deploy_to']}/shared/config/amqp.yml" do
@@ -30,7 +31,7 @@ template "#{node['redmine']['deploy_to']}/shared/config/amqp.yml" do
   variables(
       :server    => node['amqp']['server'],
       :username  => node['amqp']['user'],
-      :password  => amqp_pass,
+      :password  => node['amqp']['password'],
       :vhost     => node['amqp']['vhost']
   )
 end
