6f17d531a0bd7e227562e589b52f49bdbc0626c4
Peter Niederlag <peter.niederlag@datenbetrieb.de>
2014-03-08 11:07:04 +0100

    update attribute handling
    - use default instead of override
    - use chef_environment to set different default attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
+# generic defaults
 default['site-forgetypo3org']['ssl_certificate'] = 'wildcard.typo3.org'
 
 
@@ -5,11 +6,27 @@ default['site-forgetypo3org']['amqp']['server'] = nil
 default['site-forgetypo3org']['amqp']['user'] = nil
 default['site-forgetypo3org']['amqp']['vhost'] = nil
 
-default['site-forgetypo3org']['amqp'] = {
-  'server' => 'mq.typo3.org',
-  'user' => 'forgetypo3org',
-  'vhost' => 'infrastructure'
-}
+default['redmine']['hostname'] = 'forge.typo3.org'
+default['redmine']['branch'] = '1.4'
+default['redmine']['source']['repository'] = 'git://github.com/TYPO3-infrastructure/redmine.git'
+default['redmine']['source']['reference'] = '1.4-stable-typo3'
+
+# override defaults for production
+if node.chef_environment == 'production'
+  default['site-forgetypo3org']['amqp'] = {
+    'server' => 'mq.typo3.org',
+    'user' => 'forgetypo3org',
+    'vhost' => 'infrastructure'
+  }
+# override defaults for pre-production
+elsif node.chef_environment == 'pre-production'
+  default['site-forgetypo3org']['amqp'] = {
+    'server' => 'mq.typo3.org',
+    'user' => 'devforgetypo3org',
+    'vhost' => 'infrastructure_dev'
+  }
+  default['redmine']['hostname'] = 'dev.forge.typo3.org'
+end
 
 override['redmine']['deploy']['additional_symlinks'] = {
   'public/headerimages' => 'public/headerimages',
@@ -18,7 +35,4 @@ override['redmine']['deploy']['additional_symlinks'] = {
 override['redmine']['deploy']['additional_directories'] = %w{
   shared/public/headerimages
 }
-override['redmine']['hostname'] = 'forge.typo3.org'
-override['redmine']['branch'] = '1.4'
-override['redmine']['source']['repository'] = 'git://github.com/TYPO3-infrastructure/redmine.git'
-override['redmine']['source']['reference'] = '1.4-stable-typo3'
+
