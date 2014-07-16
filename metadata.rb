4e3c904502767a27df87d95269eb1ae59efd5e1c
Peter Niederlag <peter.niederlag@typo3.org>
2014-07-16 10:50:25 +0200

    version 0.0.13

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "steffen.gebert@typo3.org"
 license          "Apache 2.0"
 description      "Installs/Configures forge.typo3.org"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version "0.0.8"
+version "0.0.13"
 
 depends "redmine"
 depends "nginx"
