238b3b2b32721c1fe8b4b4655e02e61f51927261
Peter Niederlag <peter.niederlag@typo3.org>
2014-10-02 17:48:21 +0200

    site-forgetypo3org 0.0.19, depend on redmine ~> 0.2.0
     * redmine 0.2.0 introduces logrotation

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email "steffen.gebert@typo3.org"
 license          "Apache 2.0"
 description      "Installs/Configures forge.typo3.org"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version "0.0.18"
+version "0.0.19"
 
-depends "redmine", "~> 0.1.4"
+depends "redmine", "~> 0.2.0"
 depends "nginx"
 depends "ssl_certificates"
 depends "mysql"
