40248ec78215779a370c338a9f44e3cfb69ffa89
Steffen Gebert <steffen.gebert@typo3.org>
2015-01-18 16:06:41 +0100

    Bump dependency for redmine cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email "steffen.gebert@typo3.org"
 license          "Apache 2.0"
 description      "Installs/Configures forge.typo3.org"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version "0.0.25"
+version "0.0.26"
 
-depends "redmine",          "~> 0.2.2"
+depends "redmine",          "~> 0.2.3"
 depends "nginx",            "= 1.6.0"
 depends "ssl_certificates", "= 1.1.3"
 depends "mysql",            "= 1.3.0"
