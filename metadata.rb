622947c0971eee8477a9683266d44ab39d2da8c2
Steffen Gebert <steffen.gebert@typo3.org>
2017-04-04 08:02:05 +0200

    [TASK] Update to redmine cookbook 1.0

--- metadata.rb
+++ metadata.rb
@@ -9,9 +9,9 @@ version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0
 depends "t3-base",          "~> 0.2.0"
 
 depends "t3-mysql",         "~> 5.1.0"
-depends "redmine",          "~> 0.3.1"
+depends "redmine",          "~> 1.0.0"
 depends "ssl_certificates", "~> 1.1.3"
 depends "t3-chef-vault",    "~> 1.0.1"
 
-depends "nginx",            "~> 2.8.0"
+depends "chef_nginx",       "= 5.1.3"
 depends "php",              "= 1.1.2"
