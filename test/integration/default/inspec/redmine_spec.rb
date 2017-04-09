0db2df78a2cd7fd67a15be339b25bd9ad1c935b7
Steffen Gebert <steffen.gebert@typo3.org>
2017-04-09 08:55:06 +0200

    [TESTS] Add sleep :-(

--- test/integration/default/inspec/redmine_spec.rb
+++ test/integration/default/inspec/redmine_spec.rb
@@ -11,6 +11,9 @@ control 'redmine-1' do
     its('protocols') { should include 'tcp'}
   end
 
+  # sorry for that.. rails needs some time to boot
+  sleep 10
+
   # port 80 HTML
   describe command('curl http://localhost:80') do
     its('exit_status') { should eq 0 }
